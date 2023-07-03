4dcec650681c90a63520a0fd695100bcdbbeff03
Anup Patel <apatel@ventanamicro.com>
2023-07-03 18:48:41 +0530

    lib: utils: Add simple FDT based CPPC driver framework
    The generic platform can have multiple CPPC drivers so let us
    add a simple FDT based CPPC driver framework.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>

--- platform/generic/platform.c
+++ platform/generic/platform.c
@@ -17,6 +17,7 @@
 #include <sbi/sbi_string.h>
 #include <sbi/sbi_system.h>
 #include <sbi/sbi_tlb.h>
+#include <sbi_utils/cppc/fdt_cppc.h>
 #include <sbi_utils/fdt/fdt_domain.h>
 #include <sbi_utils/fdt/fdt_fixup.h>
 #include <sbi_utils/fdt/fdt_helper.h>
@@ -238,6 +239,7 @@ static int generic_early_init(bool cold_boot)
 	}
 
 	fdt_hsm_init(cold_boot);
+	fdt_cppc_init(cold_boot);
 
 	if (!generic_plat || !generic_plat->early_init)
 		return 0;
@@ -291,6 +293,7 @@ static int generic_vendor_ext_provider(long funcid,
 
 static void generic_early_exit(void)
 {
+	fdt_cppc_exit();
 	fdt_hsm_exit();
 
 	if (generic_plat && generic_plat->early_exit)

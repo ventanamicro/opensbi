883054cb68e3510d001ab9fbf2c6ab78da1cd8b5
Himanshu Chauhan <hchauhan@ventanamicro.com>
2023-10-23 12:55:57 +0530

    lib: utils: Introduce RAS RPMI based driver
    - Add RAS driver which communicates to RAS agent over RPMI
    - The driver is enumarated from the FDT entry
    
    Signed-off-by: Himanshu Chauhan <hchauhan@ventanamicro.com>

---
+++ lib/utils/ras/objects.mk
@@ -0,0 +1,11 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2025 Ventana Micro Systems Inc.
+#
+# Authors:
+#   Himanshu Chauhan <hchauhan@ventanamicro.com>
+#
+
+carray-fdt_early_drivers-$(CONFIG_FDT_RAS_RPMI) += fdt_ras_rpmi
+libsbiutils-objs-$(CONFIG_FDT_RAS_RPMI) += ras/fdt_ras_rpmi.o

146d417d63500f6b450bb679c7eda84db9f504c8
Himanshu Chauhan <hchauhan@ventanamicro.com>
2024-05-29 17:20:55 +0530

    lib: utils: Add support for RAS agent in OpenSBI
    The RAS agent in OpenSBI get information about various error sources
    in the system and their properties. The properties include the reri
    bank addresse, the notification type, SSE vector, etc. It parses the
    error sources and allocates buffers for APEI Error Status block for
    each error source. On an error, the reri registers of the error sources
    are read and their corresponding CPER record is generated and logged.
    
    Signed-off-by: Himanshu Chauhan <hchauhan@ventanamicro.com>

--- lib/utils/ras/objects.mk
+++ lib/utils/ras/objects.mk
@@ -11,3 +11,7 @@ carray-fdt_early_drivers-$(CONFIG_FDT_RAS_RPMI) += fdt_ras_rpmi
 libsbiutils-objs-$(CONFIG_FDT_RAS_RPMI) += ras/fdt_ras_rpmi.o
 
 libsbiutils-objs-$(CONFIG_FDT_SBI_RAS_AGENT) += ras/ghes.o
+
+carray-fdt_early_drivers-$(CONFIG_FDT_SBI_RAS_AGENT) += fdt_sbi_ras_agent
+libsbiutils-objs-$(CONFIG_FDT_SBI_RAS_AGENT) += ras/fdt_ras_agent.o
+libsbiutils-objs-$(CONFIG_FDT_SBI_RAS_AGENT) += ras/reri_drv.o
